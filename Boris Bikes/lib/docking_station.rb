6e956f9c6da2891f7c7ffa591233636551e3c449
Oliver <ofcharrison@gmail.com>
2019-02-07 16:53:34 +0000

    15. Refactoring - simplified the docking station spec

--- Boris Bikes/lib/docking_station.rb
+++ Boris Bikes/lib/docking_station.rb
@@ -1,24 +1,28 @@
-
-require 'bike'
-
-
-
 class DockingStation
   def initialize()
     @bikes = []
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
 
   def dock(bike)
-    fail 'Dock station full' if @bikes.count >= 20
+    fail 'Dock station full' if full?
     @bikes << bike
   end
 
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
 end
 
 docking_station = DockingStation.new
