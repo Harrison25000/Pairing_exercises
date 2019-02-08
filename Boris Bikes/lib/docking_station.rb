23bb9ef0f97f023a31305f8993c3cdb585c3ad4e
Oliver <ofcharrison@gmail.com>
2019-02-08 14:15:48 +0000

    16. Refactoring further, setting constant to 20

--- Boris Bikes/lib/docking_station.rb
+++ Boris Bikes/lib/docking_station.rb
@@ -1,4 +1,5 @@
 class DockingStation
+  DEFAULT_CAPACITY = 20
   def initialize()
     @bikes = []
   end
@@ -17,7 +18,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.count >= 20
+    @bikes.count >= DEFAULT_CAPACITY
   end
 
   def empty?
--- Boris Bikes/spec/docking_station_spec.rb
+++ Boris Bikes/spec/docking_station_spec.rb
@@ -17,7 +17,9 @@ describe DockingStation do
 
   describe '#dock' do
     it 'returns an error when full' do
-      20.times { subject.dock Bike.new}
+      DockingStation::DEFAULT_CAPACITY.times do
+      subject.dock Bike.new
+    end
       expect{subject.dock Bike.new}.to raise_error 'Dock station full'
     end
   end
